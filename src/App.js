b973dc3ad958eeef9076c2d342241a64001a746e
Caroline Courtney <caroline.courtney@earthwavetech.com>
2021-08-08 13:43:12 -0400

    movie list, and movie details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import MovieList from './components/movie-list';
+import MovieDetails from './components/movie-details';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/movies/", {
@@ -18,18 +21,18 @@ function App() {
     .catch(error => console.error(error))
   }, []);
 
+  const movieClicked = movie => {
+    setSelectedMovie(movie);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div>
-          {movies.map(movie => {
-            return <h2>{movie}</h2>
-          })}
-        </div>
-        <div>Movie Details</div>
+        <MovieList movies={movies} movieClicked={movieClicked} />
+        <MovieDetails movie={selectedMovie} />
       </div>
     </div>
   );
