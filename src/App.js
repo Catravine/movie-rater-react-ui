51432ee488bf7b0686230fda42749cd7ec493905
Caroline Courtney <caroline.courtney@earthwavetech.com>
2021-08-08 12:39:08 -0400

    basic styling; ready to fetch?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 165ff929e667e7d4260b026a6015df15e9429c28'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setMovies(resp))
+    .catch(error => console.error(error))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Rater</h1>
       </header>
+      <div className="layout">
+        <div>
+          {movies.map(movie => {
+            return <h2>{movie}</h2>
+          })}
+        </div>
+        <div>Movie Details</div>
+      </div>
     </div>
   );
 }
